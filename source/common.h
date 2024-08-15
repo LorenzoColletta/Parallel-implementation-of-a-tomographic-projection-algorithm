14f6c615932473d26270cf4e6b894146ef33ccce
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2024-08-15 17:57:25 +0200

    feat: define the environment's input parameters

--- source/common.h
+++ source/common.h
@@ -18,4 +18,23 @@
 #define DISTANCE_OBJECT_DETECTOR                // distance between the object's center and the detector
 #define DISTANCE_OBJECT_SOURCE                  // distance between the object's center and the source position
 
+/**
+ * The following global variables represent the input parameters needed to set up the environment.
+ * A reference to each variable value is provided by the previously defined constants.
+ */
+extern int gl_pixelDim;                         // side length of a pixel of the detector        
+extern int gl_angularTrajectory;                // total angular distance traveled by the source 
+extern int gl_positionsAngularDistance;         // angular distance between each source position
+extern int gl_objectSideLenght;                 // side length of the object
+extern int gl_detectorSideLength;               // side lenth of the detector                   
+extern int gl_distanceDetectorObject;           // distance between the object's center and the detector           
+extern int gl_distanceDetectorSource;           // distance between the object's center and the source position
+extern int gl_voxelXDim;                        // voxel side along x-axis
+extern int gl_voxelYDim;                        // voxel side along y-axis
+extern int gl_voxelZDim;                        // voxel side along z-axis
+extern int gl_nVoxel[3];                        // an array containing the number of voxel the object is composed of along the X axis [0], Y axis [1], Z axis [2]
+
+
+
+
 
