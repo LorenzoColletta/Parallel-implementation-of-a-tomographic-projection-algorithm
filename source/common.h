a7ae806871f18eca01f45f247a61bfb8a8127c5d
Lorenzo Colletta <lorenzo.colletta@studio.unibo.it>
2024-08-15 17:49:12 +0200

    feat: define input parameters value reference

---
+++ source/common.h
@@ -0,0 +1,21 @@
+#ifndef M_PI
+#define M_PI (3.14159265358979323846)
+#endif
+
+/**
+ * The following constants are input parameters values to be considered as a reference.
+ */
+#define VOXEL_X_DIM 100                         // voxel side length along x-axis
+#define VOXEL_Y_DIM 100                         // voxel side length along y-axis
+#define VOXEL_Z_DIM 100                         // voxel side length along z-axis
+
+#define PIXEL_DIM 85                            // side length of a pixel of the detector
+#define ANGULAR_TRAJECTORY 90                   // total angular distance traveled by the source 
+#define POSITIONS_ANGULAR_DISTANCE 15           // angular distance between each source position
+
+#define OBJECT_SIDE_LENGTH                      // side length of the object
+#define DETECTOR_SIDE_LENGTH                    // side lenth of the detector
+#define DISTANCE_OBJECT_DETECTOR                // distance between the object's center and the detector
+#define DISTANCE_OBJECT_SOURCE                  // distance between the object's center and the source position
+
+
